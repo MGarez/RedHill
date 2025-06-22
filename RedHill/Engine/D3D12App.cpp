fa14384f8e11e857a9d1b8e557b8597048d0b4dd
Mat Garez <matgarez@gmail.com>
2025-06-22 19:03:07 +0200

    Now with a triangle!

--- RedHill/Engine/D3D12App.cpp
+++ RedHill/Engine/D3D12App.cpp
@@ -59,6 +59,8 @@ using Microsoft::WRL::ComPtr;
 D3D12App::D3D12App(UINT width, UINT height, std::wstring name)
 	:DXSample(width, height, name),
 	m_frameIndex(0),
+	m_viewport(0.0f,0.0f, static_cast<FLOAT>(width), static_cast<FLOAT>(height)),
+	m_scissorRect(0,0, static_cast<LONG>(width), static_cast<LONG>(height)),
 	m_rtvDescriptorSize(0)
 {
 }
@@ -193,11 +195,53 @@ void D3D12App::InitAssets()
 {
 	// Create an empty root signature
 
-	// Compile the shaders
+	CD3DX12_ROOT_SIGNATURE_DESC rsDesc;
+	rsDesc.Init(0, nullptr, 0, nullptr, D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT);
+
+	ComPtr<ID3DBlob> signature;
+	ComPtr<ID3DBlob> error;
+	ThrowIfFailed(D3D12SerializeRootSignature(&rsDesc, D3D_ROOT_SIGNATURE_VERSION_1, &signature, &error));
+	ThrowIfFailed(m_device->CreateRootSignature(0, signature->GetBufferPointer(), signature->GetBufferSize(), IID_PPV_ARGS(&m_rootSignature)));
+
+	// Shader compilation
+
+	ComPtr<ID3DBlob> vShader;
+	ComPtr<ID3DBlob> pShader;
+
+#if defined(_DEBUG)
+	// Enable better shader debugging with the graphics debugging tools.
+	UINT compileFlags = D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION;
+#else
+	UINT compileFlags = 0;
+#endif
+
+	ThrowIfFailed(D3DCompileFromFile(GetAssetFullPath(L"TriangleShader.hlsl").c_str(), nullptr, nullptr, "VSMain", "vs_5_0", compileFlags, 0, &vShader, nullptr));
+	ThrowIfFailed(D3DCompileFromFile(GetAssetFullPath(L"TriangleShader.hlsl").c_str(), nullptr, nullptr, "PSMain", "ps_5_0", compileFlags, 0, &pShader, nullptr));
 
 	// Create the vertex input layout
+	D3D12_INPUT_ELEMENT_DESC inputDesc[] =
+	{
+		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+		{ "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 }
+	};
 
 	// Create a pipeline state object description, then the object
+
+	D3D12_GRAPHICS_PIPELINE_STATE_DESC psoDesc = {};
+	psoDesc.InputLayout = { inputDesc, _countof(inputDesc) };
+	psoDesc.pRootSignature = m_rootSignature.Get();
+	psoDesc.VS = CD3DX12_SHADER_BYTECODE(vShader.Get());
+	psoDesc.PS = CD3DX12_SHADER_BYTECODE(pShader.Get());
+	psoDesc.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
+	psoDesc.BlendState = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
+	psoDesc.DepthStencilState.DepthEnable = FALSE;
+	psoDesc.DepthStencilState.StencilEnable = FALSE;
+	psoDesc.SampleMask = UINT_MAX;
+	psoDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+	psoDesc.NumRenderTargets = 1;
+	psoDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM;
+	psoDesc.SampleDesc.Count = 1;
+	ThrowIfFailed(m_device->CreateGraphicsPipelineState(&psoDesc, IID_PPV_ARGS(&m_pipelineState)));
 
 	// Create the command list
 	ThrowIfFailed(m_device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, m_commandAllocator.Get(), nullptr, IID_PPV_ARGS(&m_commandList)));
@@ -208,8 +252,34 @@ void D3D12App::InitAssets()
 	m_commandList->Close();
 	 
 	// Create and load the vertex buffers
+	Vertex triangleVertices[] =
+	{
+		{ { 0.0f, 0.25f * m_aspectRatio, 0.0f }, { 1.0f, 0.0f, 0.0f, 1.0f } },
+		{ { 0.25f, -0.25f * m_aspectRatio, 0.0f }, { 0.0f, 1.0f, 0.0f, 1.0f } },
+		{ { -0.25f, -0.25f * m_aspectRatio, 0.0f }, { 0.0f, 0.0f, 1.0f, 1.0f } }
+	};
+
+	const UINT vertexBufferSize = sizeof(triangleVertices);
+
+	// Note: using upload heaps to transfer static data like vert buffers is not recommended
+	auto heap_properties = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD);
+	auto buffer_desc = CD3DX12_RESOURCE_DESC::Buffer(vertexBufferSize);
+
+
+	ThrowIfFailed(m_device->CreateCommittedResource(&heap_properties, D3D12_HEAP_FLAG_NONE, &buffer_desc, D3D12_RESOURCE_STATE_GENERIC_READ, NULL, IID_PPV_ARGS(&m_vertexBuffer)));
+
+	// Copy the data to the vertex buffer
+	UINT8* pVertexDataBegin;
+	CD3DX12_RANGE readRange(0, 0); 
+	ThrowIfFailed(m_vertexBuffer->Map(0, &readRange, reinterpret_cast<void**>(&pVertexDataBegin)));
+	memcpy(pVertexDataBegin, triangleVertices, sizeof(triangleVertices));
+	m_vertexBuffer->Unmap(0, nullptr);
 
 	// Create the vertex buffer views
+
+	m_vertexBufferView.BufferLocation = m_vertexBuffer->GetGPUVirtualAddress();
+	m_vertexBufferView.StrideInBytes = sizeof(Vertex);
+	m_vertexBufferView.SizeInBytes = vertexBufferSize;
 
 	// Create a fence
 
@@ -238,19 +308,29 @@ void D3D12App::PopulateCommandList()
 	ThrowIfFailed(m_commandList->Reset(m_commandAllocator.Get(), m_pipelineState.Get()));
 
 	// Set the graphics root signature
-	
+	m_commandList->SetGraphicsRootSignature(m_rootSignature.Get());
+
 	// Set the viewport and scissor rectangles
+
+	m_commandList->RSSetViewports(1, &m_viewport);
+	m_commandList->RSSetScissorRects(1, &m_scissorRect);
 
 	// Set a resource barrier, indicating the back buffer is to be used as a render target
 	
 	CD3DX12_RESOURCE_BARRIER rt_barrier = CD3DX12_RESOURCE_BARRIER::Transition(m_renderTargets[m_frameIndex].Get(), D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET);
 	m_commandList->ResourceBarrier(1, &rt_barrier);
 
+
+	CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(m_rtvHeap->GetCPUDescriptorHandleForHeapStart(), m_frameIndex, m_rtvDescriptorSize);
+	m_commandList->OMSetRenderTargets(1, &rtvHandle, FALSE, nullptr);
+
 	// Record commands into the command list
 
 	const float clearColor[] = { 1.0f, 0.2f, 0.6f, 1.0f };
-	CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(m_rtvHeap->GetCPUDescriptorHandleForHeapStart(), m_frameIndex, m_rtvDescriptorSize);
 	m_commandList->ClearRenderTargetView(rtvHandle, clearColor, 0, nullptr);
+	m_commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	m_commandList->IASetVertexBuffers(0, 1, &m_vertexBufferView);
+	m_commandList->DrawInstanced(3, 1, 0, 0);
 
 	// Indicate the back buffer will be used to present after the command list has executed
 	CD3DX12_RESOURCE_BARRIER pst_barrier = CD3DX12_RESOURCE_BARRIER::Transition(m_renderTargets[m_frameIndex].Get(), D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT);
@@ -262,7 +342,7 @@ void D3D12App::PopulateCommandList()
 
 void D3D12App::WaitForPreviousFrame()
 {
-	// Waiting for frame is not a best practise. This is temporal before implementing frame buffering
+	// Waiting for frame is not a best practice. This is temporal before implementing frame buffering
 
 	// Signal and increment fence value
 	const UINT64  fence = m_fenceValue;
--- RedHill/Engine/D3D12App.h
+++ RedHill/Engine/D3D12App.h
@@ -2,6 +2,7 @@
 
 #include "DXSample.h"
 
+using namespace DirectX;
 using Microsoft::WRL::ComPtr;
 
 class D3D12App : public DXSample
@@ -15,18 +16,34 @@ public:
     virtual void OnDestroy() override;
 
 private:
+
     static const UINT FrameCount = 2;
 
+    struct Vertex
+    {
+        XMFLOAT3 position;
+        XMFLOAT4 color;
+    };
+
     // Pipeline objects.
+
+    CD3DX12_VIEWPORT m_viewport;
+    CD3DX12_RECT m_scissorRect;
+
     ComPtr<IDXGISwapChain3> m_swapChain;
     ComPtr<ID3D12Device> m_device;
     ComPtr<ID3D12Resource> m_renderTargets[FrameCount];
     ComPtr<ID3D12CommandAllocator> m_commandAllocator;
     ComPtr<ID3D12CommandQueue> m_commandQueue;
+    ComPtr<ID3D12RootSignature> m_rootSignature;
     ComPtr<ID3D12DescriptorHeap> m_rtvHeap;
     ComPtr<ID3D12PipelineState> m_pipelineState;
     ComPtr<ID3D12GraphicsCommandList> m_commandList;
     UINT m_rtvDescriptorSize;
+
+    // App resources
+    ComPtr<ID3D12Resource> m_vertexBuffer;
+    D3D12_VERTEX_BUFFER_VIEW m_vertexBufferView;
 
     // Synchronization objects.
     UINT m_frameIndex;
